ebe08efb1ee707d2f407f4e5e22010ff21b7d4e3
FrankJason12138 <FrankJason12138.github.io>
2024-01-02 18:55:20 +0800

    设置漏答逻辑

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 document.getElementById("psychologyTest").onsubmit = function(event) {
-    event.preventDefault();
+    // 首先检查是否所有问题都已回答
+    var questions = document.querySelectorAll('.question-group');
+    var isAllAnswered = true;
 
+    questions.forEach(function(question) {
+        var inputs = question.querySelectorAll('input[type="radio"]');
+        var isChecked = Array.from(inputs).some(input => input.checked);
+
+        if (!isChecked) {
+            isAllAnswered = false;
+        }
+    });
+
+    // 如果有问题未回答，显示警告并阻止表单提交
+    if (!isAllAnswered) {
+        event.preventDefault();
+        alert('对不起，请检查漏掉的题目');
+        return; // 阻止继续执行后面的代码
+    }
+
+    // 如果所有问题都已回答，计算分数
     var score = 0;
     var form = document.getElementById("psychologyTest");
     var answers = form.elements;
@@ -13,4 +32,8 @@ document.getElementById("psychologyTest").onsubmit = function(event) {
 
     var result = "您的得分是: " + score;
     document.getElementById("result").innerText = result;
+
+    // 阻止表单的默认提交行为
+    event.preventDefault();
 };
+
