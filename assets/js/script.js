497d55896072b6d75e0e0b23248ff30a045b391a
psych010gy <psych010gy.github.io>
2024-05-20 19:01:52 +0800

    修改文本

--- assets/js/script.js
+++ assets/js/script.js
@@ -968,7 +968,7 @@
                 }
             });
         }
-        resultDisplay.innerHTML += "<h4>*测评结果只对受测者最近情况进行解释，不具备临床经验；如需获得测评报告请<a href='https://www.xiaohongshu.com/goods-detail/65fea7533b4dcf0001b54344?xhsshare=CopyLink&appuid=643b847b000000000e01d766&apptime=1716200088' target='_blank' style='color: blue;'>[将结果截图后点击跳转]</a>,心理咨询师会与您沟通后生成心理测评报告并给予你咨询建议哦^_^</h4>";
+        resultDisplay.innerHTML += "<h4>*测评结果只对受测者最近情况进行解释，不具备临床经验；如需获得测评报告请<a href='https://www.xiaohongshu.com/goods-detail/65fea7533b4dcf0001b54344?xhsshare=CopyLink&appuid=643b847b000000000e01d766&apptime=1716200088' target='_blank' style='color: blue;'>[将结果截图后点击跳转]</a><strong>注意：由于量表结果为个人隐私，后台不会存储用户数据，截图保存结果后跳转</strong>心理咨询师会与您沟通后生成心理测评报告并给予你咨询建议哦^_^</h4>";
         resultDisplay.style.textAlign = "center";
         
         
