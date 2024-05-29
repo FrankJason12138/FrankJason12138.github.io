fc5037298c74f373f751592fb8dd8ca449ec2266
psych010gy <psych010gy.github.io>
2024-05-29 13:11:51 +0800

    添加PayPal按钮

--- assets/js/script.js
+++ assets/js/script.js
@@ -968,8 +968,29 @@
                 }
             });
         }
+        var resultDisplay = document.getElementById('resultDisplay');
+
+        // 设置结果内容
         resultDisplay.innerHTML += "<h4>*测评结果只对受测者最近情况进行解释，不具备临床经验；如需获得测评报告请<br><a href='https://www.xiaohongshu.com/goods-detail/65fea7533b4dcf0001b54344?xhsshare=CopyLink&appuid=643b847b000000000e01d766&apptime=1716200088' target='_blank' style='color: blue;'>[将结果截图后点击跳转]</a><br>海外用户请点击下方的PayPal支付按钮<br>注意：由于量表结果为个人隐私，后台不会存储用户数据，截图保存结果后跳转，心理咨询师会与您沟通后生成心理测评报告并给予你咨询建议哦^_^</h4>";
+        
+        // 设置结果显示区域居中对齐
         resultDisplay.style.textAlign = "center";
+        
+        // 创建一个iframe元素
+        var iframeElement = document.createElement('iframe');
+        
+        // 设置iframe的src属性
+        iframeElement.src = "paypal35usd.html";
+        
+        // 设置iframe的宽度和高度
+        iframeElement.width = "100%";
+        iframeElement.height = "100%";
+        
+        // 设置iframe的frameborder属性
+        iframeElement.frameborder = "0";
+        
+        // 将iframe插入到页面中
+        resultDisplay.appendChild(iframeElement);
 
 
 
